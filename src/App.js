2e7e4160d7f1e2c578bea97798b32b1b723f2a3c
Jose Fernandez <jose@proudcloud.net>
2019-05-23 09:08:15 +0800

    Add login validation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { Layout } from 'antd'
 import HeaderContainer from 'app/header/'
--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,39 +1,54 @@
 import React from "react";
 import useForm from "utils/useForm"
+import validateLogin from 'utils/LoginFormValidationRules';
+import 'login/index.scss'
+import Button from 'components/button'
 
 const Login = () => {
-  const { values, handleChange, handleSubmit } = useForm(login);
+  const {
+    values,
+    errors,
+    handleChange,
+    handleSubmit,
+  } = useForm(login, validateLogin);
 
-  function login(){
-    //objects of email and password
+  function login() {
     console.log(values)
+    console.log('Walang error');
   }
 
   return (
-    <div className="section is-fullheight">
-      <div className="container">
-        <div className="column is-4 is-offset-4">
-          <div className="box">
-            <form onSubmit={handleSubmit}>
-              <div className="field">
-                <label className="label">Email Address</label>
-                <div className="control">
-                  <input className="input" type="email" name="email" onChange={handleChange} required/>
+    <div>
+      <div>
+        <div>
+          <div>
+            <form onSubmit={handleSubmit} noValidate>
+              <div>
+                <label>Email Address</label>
+                <div>
+                  <input type="email" name="email" onChange={handleChange} value={values.email || ''} required />
+                  {errors.email && (
+                    <p className="help is-danger">{errors.email}</p>
+                  )}
                 </div>
               </div>
-              <div className="field">
-                <label className="label">Password</label>
-                <div className="control">
-                  <input className="input" type="password" name="password" onChange={handleChange} required />
+              <div>
+                <label>Password</label>
+                <div>
+                  <input type="password" name="password" onChange={handleChange} value={values.password || ''} required />
                 </div>
+                {errors.password && (
+                  <p>{errors.password}</p>
+                )}
               </div>
-              <button type="submit" className="button is-block is-info is-fullwidth">Login</button>
+              <button>Login</button>
             </form>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default Login
