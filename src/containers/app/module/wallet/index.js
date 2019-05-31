6be6fcc78f029e88cca43eb1b75627859e4359b8
erson lloyd <elcfrancisco11@gmail.com>
2019-05-31 12:11:06 +0800

    Add Tabs in Wallet page

--- src/containers/app/module/wallet/index.js
+++ src/containers/app/module/wallet/index.js
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { compose, withApollo, Query } from 'react-apollo'
+import { Tabs } from 'antd'
 
 import WALLET from '../../../../../src/graphql/wallet'
+import '../wallet/index.scss'
+
+const { TabPane } = Tabs;
 
 const WalletContainer = (props) => {
   const walletAddress = localStorage.getItem('walletAddress')
   return(
-    <div>
-      <Query query={WALLET.GET_WALLET_BALANCE} variables={{ walletAddress }}>
-        {({ data, loading, error }) => {
-          if (loading) return <p> Loading </p>
-          if (error) return <p>ERROR</p>
-          return(
-            <div>
-              <div>XEM BALANCE : {data.getWalletBalance.balance.xem}</div>
-              <div>GWX BALANCE : {data.getWalletBalance.balance.gwx}</div>
+    <Fragment>
+      <div className="body-container">
+        <Tabs tabPosition='left'>
+          <TabPane tab='My Wallet' key='1'>
+            <div className="body-content">
+              <Query query={WALLET.GET_WALLET_BALANCE} variables={{ walletAddress }}>
+                {({ data, loading, error }) => {
+                  if (loading) return <p> Loading </p>
+                  if (error) return <p>ERROR</p>
+                  return(
+                    <div>
+                      <div>XEM BALANCE : {data.getWalletBalance.balance.xem}</div>
+                      <div>GWX BALANCE : {data.getWalletBalance.balance.gwx}</div>
 
+                    </div>
+                  )
+                }}
+              </Query>
             </div>
-          )
-        }}
-      </Query>
-    </div>
+          </TabPane>
+          <TabPane tab='Transaction History' key='2'>
+            <span>Transactions.</span>
+          </TabPane>
+        </Tabs>
+     </div>
+    </Fragment>
   )
 }
 
