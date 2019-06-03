80266fd7a32e1b7e49a97adf805e2fc15a33ed26
fernandezmj <fernandez.michaeljohn@gmail.com>
2019-06-03 17:09:36 +0800

    fixes gamelist ui

--- src/containers/app/module/wallet/index.js
+++ src/containers/app/module/wallet/index.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react'
 import { compose, withApollo, Query } from 'react-apollo'
-import { Tabs, Card, Button } from 'antd'
+import { Spin, Tabs, Card, Button, Icon } from 'antd'
 
 import TransactionContainer from 'app/module/transaction'
 import WALLET from '../../../../../src/graphql/wallet'
 import '../wallet/index.scss'
+import {ReactComponent as SampleGame} from 'assets/images/sample-game.svg'
 
 const { TabPane } = Tabs;
 
@@ -13,39 +14,87 @@ const WalletContainer = (props) => {
   return(
     <Fragment>
       <div className="body-container">
+        <h1 className='header'>Bruce Lee's Personal Wallet</h1>
         <Tabs tabPosition='left'>
-          <TabPane tab='My Wallet' key='1'>
+          <TabPane tab={<span><Icon type='wallet' />My Wallet</span>} key='1'>
             <div className="body-content">
-              Wallet
+              <h2 className='title'>Wallet</h2>
               <Card>
-              Wallet Balance
-                <Query query={WALLET.GET_WALLET_BALANCE} variables={{ walletAddress }} fetchPolicy='network-only'>
-                  {({ data, loading, error }) => {
-                    if (loading) return <p> Loading </p>
-                    if (error) return <p>ERROR</p>
-                    const convertedBalance = (data.getWalletBalance.balance.gwx + "").split(".")
-                    return(
+                <div className='wallet-container'>
+                  <div className='top'>Wallet Balance</div>
+                  <Query query={WALLET.GET_WALLET_BALANCE} variables={{ walletAddress }} fetchPolicy='network-only'>
+                    {({ data, loading, error }) => {
+                      if (loading) return <p> Loading </p>
+                      if (error) return <p>ERROR</p>
+                      const convertedBalance = (data.getWalletBalance.balance.gwx + "").split(".")
+                      return(
                       <div>
                         <div>
                           GWX BALANCE :
                           <span style={{ fontSize: '64px' }}>{convertedBalance[0]}</span><span>.</span><span style={{ fontSize: '20px' }}>{convertedBalance[1]}</span>
-                          <Button class='primary-btn'> Add Funds+ </Button>
                         </div>
                       </div>
-                    )
-                  }}
-                </Query>
+                      )
+                    }}
+                  </Query>
+                  <Button className='primary-btn'> Add Funds+ </Button>
+                </div>
               </Card>
-              Game List
+              <h2 className='title'>Game List</h2>
               <Card>
-                <Card> Crypto Keno <Button className='primary-btn'> Add Funds+ </Button>
-                 </Card>
-                <Card> Shaker  <Button className='primary-btn'> Add Funds+ </Button> 
-                </Card>
+                <div className='game-card'> 
+                  <div className='info'>
+                    <div className='image'>
+                      <SampleGame />
+                    </div>
+                    <div className='game-details'>
+                      <h1 className='title'>
+                        Crypto Keno
+                      </h1>
+                      <div className='time'>
+                        <p>87 hrs on record</p>
+                        <p>last played on Dec, 2018</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='right'>
+                    <h2 className='item'>
+                      GWX 225
+                    </h2>
+                    <div className='action'>
+                      + Add Funds
+                    </div>
+                  </div>
+                 </div>
+                 <div className='card-divider'></div>
+                 <div className='game-card'> 
+                  <div className='info'>
+                    <div className='image'>
+                      <SampleGame />
+                    </div>
+                    <div className='game-details'>
+                      <h1 className='title'>
+                        Crypto Keno
+                      </h1>
+                      <div className='time'>
+                        <p>87 hrs on record</p>
+                        <p>last played on Dec, 2018</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='right'>
+                    <h2 className='item'>
+                      GWX 225
+                    </h2>
+                    <div className='action'>
+                      + Add Funds
+                    </div>
+                  </div>
+                 </div>
               </Card>
             </div>
           </TabPane>
-          <TabPane tab='Transaction History' key='2'>
+          <TabPane tab={<span><Icon type='table' />Transaction History</span>} key='2'>
             <TransactionContainer />
           </TabPane>
         </Tabs>
