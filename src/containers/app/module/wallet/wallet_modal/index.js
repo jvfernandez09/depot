ff4abaeecce878cc6859882e712d07d6bd465f8a
Jose Fernandez <jose@proudcloud.net>
2019-06-11 13:29:27 +0800

    Refactor Wallet Modal

--- src/containers/app/module/wallet/wallet_modal/index.js
+++ src/containers/app/module/wallet/wallet_modal/index.js
@@ -1,18 +1,116 @@
-import React, { Fragment} from 'react';
-import { Modal, Input, Select, Button } from 'antd'
+import React, { Fragment, useState } from 'react';
+import { Steps, message, Modal, Input, Select, Button } from 'antd'
 import useModal from 'app/module/wallet/wallet_modal/useModal'
 import '../../wallet/wallet_modal/index.scss'
 
+import {ReactComponent as SampleQR} from 'assets/images/sample_qr_code.svg'
+
 const { Option } = Select
+const { Step } = Steps
 
 const WalletModal = ({ isShowing, hide, walletAddress }) => {
   const { handleChange, handleChangeSelect, inputs, handleSubmit, isLoading } = useModal(addFunds)
+  const [isShowQr, isSetShowQr] = useState(false)
+  const [done, setDone] = useState(false)
+  const [current, setCurrent] = useState(0)
+  const steps = [
+    {
+      title: 'Top up',
+      content: 'Top up',
+    },
+    {
+      title: 'Generate QR',
+      content: 'Generate QR',
+    },
+    {
+      title: 'Done',
+      content: 'Done',
+    },
+  ]
+
+  function next() {
+    const count = current + 1
+      if(count === 1){
+        handleSubmit()
+      } else {
+        confirmTransaction()
+        isSetShowQr(false)
+        setDone(true)
+      }
+    setCurrent(count)
+  }
+
+  function prev() {
+    const count = current - 1
+    isSetShowQr(false)
+    setCurrent(count)
+  }
+
+
+  function doneTransaction(){
+    isSetShowQr(false)
+    setDone(false)
+    setCurrent(0)
+    hide()
+  }
 
   function addFunds(){
     const variables = { ...inputs, walletAddress}
     console.log(variables)
-    hide()
+    isSetShowQr(true)
+    generateQR(variables)
   }
+
+  function generateQR(response){
+    return (
+      <div>
+       <SampleQR />
+      </div>
+    )
+  }
+
+  function formInput(){
+    return (
+      <div>
+        <div>
+          <span >Your wallet address {'*'.repeat(walletAddress.length)} </span>
+        </div>
+
+        <div>
+          <span> Amount: </span>
+          <Input
+            name='amount'
+            type='number'
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <span> Payment Method: </span>
+          <Select
+            placeholder='Select Payment Method'
+            name='type'
+            style={{ width: '100%'}}
+            onChange={handleChangeSelect}
+            >
+            <Option value="BTC">Bitcoin</Option>
+            <Option value="XEM">XEM</Option>
+            <Option value="Etherium">Etherium</Option>
+        </Select>
+        </div>
+      </div>
+    )
+  }
+
+
+  function confirmTransaction(){
+    return(
+      <div>
+        Your Transaction is Pending
+      </div>
+    )
+  }
+
   return(
     <Fragment>
       {isShowing ? (
@@ -20,46 +118,44 @@ const WalletModal = ({ isShowing, hide, walletAddress }) => {
           title='Top Up Wallet'
           visible={isShowing}
           onCancel={hide}
-          footer={[
-            <Button
-              key="2"
-              type="primary"
-              onClick={hide}>Cancel
-            </Button>,
-            <Button
-              key="1"
-              type="primary"
-              loading={isLoading}
-              onClick={handleSubmit}>Add Funds
+          footer={[current > 0 && (
+            <Button key="3" style={{ marginLeft: 8 }} onClick={() => prev()}>
+              Previous
             </Button>
-            ]
-          }>
+            ),
+            current < steps.length - 1 && (
+              <Button key="1" type="primary" onClick={() => next()}>
+                Next
+              </Button>
+            ),
+            current === steps.length - 1 && (
+              <Button key="2" type="primary" onClick={() => doneTransaction()}>
+                Done
+              </Button>
+            )
+          ]}>
 
-          <div>
-            <span >Your wallet address {'*'.repeat(walletAddress.length)} </span>
-          </div>
-
-          <div>
-            <span> Amount: </span>
-            <Input
-              name='amount'
-              type='number'
-              onChange={handleChange}
-            />
-          </div>
-
-          <div>
-            <span> Payment Method: </span>
-            <Select
-              placeholder='Select Payment Method'
-              name='type'
-              style={{ width: '100%'}}
-              onChange={handleChangeSelect}
-              >
-              <Option value="BTC">Bitcoin</Option>
-              <Option value="XEM">XEM</Option>
-              <Option value="Etherium">Etherium</Option>
-          </Select>
+          <Steps current={current}>
+            {steps.map(item => (
+              <Step key={item.title} title={item.title} />
+            ))}
+          </Steps>
+          <div className="steps-action">
+            {current < steps.length - 1 && isShowQr === false ? (
+              <div>
+                {formInput()}
+              </div>
+            ) : null }
+            {current > 0 && isShowQr ?  (
+              <div>
+                {generateQR()}
+              </div>
+            ) : null }
+            { current === steps.length - 1 &&  done ? (
+              <div>
+                {confirmTransaction()}
+              </div>
+            ) : null}
           </div>
         </Modal>
         ) : null
