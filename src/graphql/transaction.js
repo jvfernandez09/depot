c7501e570d74b8e248965fc4554f4dbfc3d511b9
Jose Fernandez <jose@proudcloud.net>
2019-07-18 15:04:10 +0800

    Add mutation for conversion of BTC and create top up transaction

--- src/graphql/transaction.js
+++ src/graphql/transaction.js
@@ -6,7 +6,7 @@ const ALL_TRANSACTIONS = gql`
     getWalletAccount(userId: $userId)
     @rest(
       type: "Transactions",
-      path: "/top_up_transactions/{args.userId}",
+      path: "/v1/top_up_transactions?user_id={args.userId}",
       method: "GET",
       endpoint: "v2"
     ) {
@@ -19,8 +19,8 @@ const CREATE_TRANSACTION = gql`
   mutation CREATE_TRANSACTION {
     createTransaction(input: $input)
     @rest(
-      type: "Transaction",
-      path: "/top_up_transactions",
+      type: "Create Transaction",
+      path: "/v1/top_up_transactions",
       method: "POST",
       endpoint: "v2"
     ) {
@@ -30,7 +30,25 @@ const CREATE_TRANSACTION = gql`
   }
 `
 
+const CONVERT_BTC = gql`
+  query convertBtc {
+    convertAmount(btc: $btc)
+    @rest(
+      type: "Convert BTC",
+      path: "/v1/top_up_transactions/calculate/btc/{args.btc}/to_gwx",
+      method: "GET",
+      endpoint: "v2"
+    ) {
+      btc
+      gwx
+    }
+  }
+`
+
+
+
 export default {
   ALL_TRANSACTIONS,
-  CREATE_TRANSACTION
+  CREATE_TRANSACTION,
+  CONVERT_BTC
 }
