ed5cdd27d4e6bb4e654878437712bf9b3b334357
Jose Fernandez <jose@proudcloud.net>
2019-10-03 15:09:53 +0800

    Revery currency to 0.16 PHP

--- src/containers/app/header/rates/index.js
+++ src/containers/app/header/rates/index.js
@@ -23,16 +23,16 @@ const Rate = (props) => {
     client.query({
       query: REAL_TIME_RATES
     }).then((result) => {
-      let btc = 0.019 / parseFloat(result.data.rates.data.attributes.btc_rate)
+      let btc = 0.0031 / parseFloat(result.data.rates.data.attributes.btc_rate)
       let btc_rate = btc * 0.02
       btc_rate = btc_rate + btc
-      let eth = 0.019 / parseFloat(result.data.rates.data.attributes.eth_rate)
+      let eth = 0.0031 / parseFloat(result.data.rates.data.attributes.eth_rate)
       let eth_rate = eth * 0.02
       eth_rate = eth_rate + eth
-      let xem = 0.019 / parseFloat(result.data.rates.data.attributes.xem_rate)
+      let xem = 0.0031 / parseFloat(result.data.rates.data.attributes.xem_rate)
       let xem_rate = xem * 0.02
       xem_rate = xem_rate + xem
-      
+
       setBtc(btc_rate.toFixed(8))
       setEth(eth_rate.toFixed(8))
       setXem(xem_rate.toFixed(6))
@@ -44,7 +44,7 @@ const Rate = (props) => {
   return (
     <>
       {getConversion()}
-      <span style={{ color: '#F8D154'}}> BUY 1 GWX: 0.019 USD<span style={{ marginLeft: '8px', marginRight: '8px' }}>•</span></span>
+      <span style={{ color: '#F8D154'}}> BUY 1 GWX: 0.0031 USD<span style={{ marginLeft: '8px', marginRight: '8px' }}>•</span></span>
       <span style={{ color: '#F8D154'}}> {btc}  BTC <span style={{ marginLeft: '8px', marginRight: '8px' }}>•</span></span>
       <span style={{ color: '#F8D154'}}> {eth}  ETH <span style={{ marginLeft: '8px', marginRight: '8px' }}>•</span></span>
       <span style={{ color: '#F8D154'}}> {xem}  XEM </span>
