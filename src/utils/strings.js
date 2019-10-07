ecef8d359562b9f637432408cf4787d4297e6e3d
Jose Fernandez <jose@proudcloud.net>
2019-10-07 16:52:37 +0800

    Add object for strings used in the app

---
+++ src/utils/strings.js
@@ -0,0 +1,26 @@
+const strings = {
+  email_address: 'Email Address',
+  email: 'Email',
+  password: 'Password',
+  new_password: 'New Password',
+  confirm_password: 'Confirm Password',
+  temporary_password: 'Send Temporary Password',
+  forgot_password: 'Forgot password?',
+  no_account: "Don't have any account",
+  register_now: ' Register Now!',
+
+  personal_wallet: 'PERSONAL WALLET',
+  user_details: 'User Details',
+  last_logged_in: 'Last logged in:',
+  wallet_address: 'Wallet Address:',
+  my_wallets: 'My Wallets',
+  transaction_history: 'Transaction History',
+
+  my_gwx_wallet: 'My GWX Wallet',
+  my_game_wallet: 'My Game Wallets',
+  no_game: "You don't have any Game Wallets.",
+
+  confirm: 'Confirm'
+}
+
+export default strings
