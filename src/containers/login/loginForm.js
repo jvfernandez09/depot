2322eff24c9934cd71213a08fcaf9ab8d6f62850
erson lloyd <elcfrancisco11@gmail.com>
2019-10-31 12:20:44 +0800

    Fixes Login UI

--- src/containers/login/loginForm.js
+++ src/containers/login/loginForm.js
@@ -6,8 +6,9 @@ import PropTypes from 'prop-types'
 import strings from 'utils/strings'
 import Input from 'components/input'
 import Button from 'components/button'
-import { ReactComponent as Logo } from 'assets/images/LOGO-tokendepot.svg'
+import { ReactComponent as Logo } from 'assets/images/token-depot.svg'
 import 'login/index.scss'
+import { Form } from 'antd'
 
 const LoginForm = ({
   inputs,
@@ -36,8 +37,12 @@ const LoginForm = ({
               YOUR ACCOUNT IN GAMEWORKS
             </div>
           </div>
-            <form>
-              <div className='form-group'>
+            <Form>
+              <Form.Item
+                className='form-group'
+                validateStatus={errors.email ? 'error' : ''}
+                help={errors.email ? errors.email : ''}
+              >
                 <label>{strings.email_address}</label>
                 <div>
                   <Input
@@ -48,13 +53,13 @@ const LoginForm = ({
                     value={inputs.email || ''}
                     required
                   />
-                  {errors.email && (
-                    <p style={{ color: 'red'}}>{errors.email}</p>
-                  )}
                 </div>
-              </div>
-
-              <div className='form-group'>
+              </Form.Item>
+              <Form.Item
+                className='form-group'
+                validateStatus={errors.password ? 'error' : ''}
+                help={errors.password ? errors.password : ''}
+              >
                 <label>{strings.password}</label>
                 <div>
                   <Input
@@ -66,26 +71,25 @@ const LoginForm = ({
                     required
                   />
                 </div>
-                {errors.password && (
-                  <p style={{ color: 'red'}}>{errors.password}</p>
-                )}
                 <div className='forgot-pass'>
                   <a href='/reset'> {strings.forgot_password}</a>
                 </div>
-              </div>
-              <Button
-                onClick={handleSubmit}
-                loading={isLoading}
-                className='button btn-primary btn-block btn-lg'
-              >Login
-              </Button>
+              </Form.Item>
+              <Form.Item>
+                <Button
+                  onClick={handleSubmit}
+                  loading={isLoading}
+                  className='button btn-primary btn-block btn-lg'
+                >Login
+                </Button>
+              </Form.Item>
               <div className='form-group'>
                 <div className='link-action'>
                   <p> {strings.no_account}</p>
                   <Link to="/register">{strings.register_now}</Link>
                 </div>
               </div>
-            </form>
+            </Form>
           </div>
         </div>
       </div>
