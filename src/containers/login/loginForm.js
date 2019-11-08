f14c249e3fd97f8cfc38634c5e6475c2f59026b6
Jose Fernandez <jose@proudcloud.net>
2019-11-08 11:55:21 +0800

    Fix UI for login page and logout

--- src/containers/login/loginForm.js
+++ src/containers/login/loginForm.js
@@ -17,7 +17,6 @@ const LoginForm = ({
   handleSubmit,
   isLoading }) => {
 
-
   function onKeyPress(e){
     if(e.which === 13) {
       e.preventDefault()
@@ -27,72 +26,73 @@ const LoginForm = ({
 
   return (
     <div className='container session-layout'>
-      <div className='item'>
-        <a href ="/"> <Logo className='logo' /> </a>
-        <div className='session-container'>
-          <div className='content'>
+    <div className='item'>
+      <Link to='/'>
+        <Logo className="logo" />
+      </Link>
+      <div className='session-container'>
+        <div className='content'>
           <div className='head -smallpad'>
-            <h1 className='title'>Log in</h1>
+            <h1 className='title'>{strings.log_in}</h1>
             <div className='sub'>
-              YOUR ACCOUNT IN GAMEWORKS
+              {strings.yourAccountInGameworks}
             </div>
           </div>
-            <Form>
-              <Form.Item
-                className='form-group'
-                validateStatus={errors.email ? 'error' : ''}
-                help={errors.email ? errors.email : ''}
+          <Form>
+            <Form.Item
+              className='form-group'
+              validateStatus={errors.email ? 'error' : ''}
+              help={errors.email ? errors.email : ''}
+            >
+              <label>{strings.email_address}</label>
+              <Input
+                type="email"
+                name="email"
+                value={inputs.email || ''}
+                onChange={handleChange}
+                onKeyPress={onKeyPress}
+                required
+              />
+            </Form.Item>
+            <Form.Item
+              className='form-group -nopad'
+              validateStatus={errors.password ? 'error' : ''}
+              help={errors.password ? errors.password : ''}
+            >
+              <label>{strings.password}</label>
+              <Input
+                type="password"
+                name="password"
+                required
+                value={inputs.password || ''}
+                onChange={handleChange}
+                onKeyPress={onKeyPress}
+              />
+            </Form.Item>
+            {(errors && errors === 'Invalid Credentials') && (
+              <p className="help is-danger">{errors}</p>
+            )}
+            <div className='others'>
+              <Link to='/reset'>{strings.forgot_password}</Link>
+            </div>
+            <Form.Item className='button-form'>
+              <Button
+                className='button btn-primary btn-block btn-lg btn-submit'
+                onClick={handleSubmit}
+                loading={isLoading}
+                htmlType="submit"
               >
-                <label>{strings.email_address}</label>
-                <div>
-                  <Input
-                    type="text"
-                    name="email"
-                    onChange={handleChange}
-                    onKeyPress={onKeyPress}
-                    value={inputs.email || ''}
-                    required
-                  />
-                </div>
-              </Form.Item>
-              <Form.Item
-                className='form-group'
-                validateStatus={errors.password ? 'error' : ''}
-                help={errors.password ? errors.password : ''}
-              >
-                <label>{strings.password}</label>
-                <div>
-                  <Input
-                    type="password"
-                    name="password"
-                    onChange={handleChange}
-                    onKeyPress={onKeyPress}
-                    value={inputs.password || ''}
-                    required
-                  />
-                </div>
-                <div className='forgot-pass'>
-                  <a href='/reset'> {strings.forgot_password}</a>
-                </div>
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  onClick={handleSubmit}
-                  loading={isLoading}
-                  className='button btn-primary btn-block btn-lg'
-                >{strings.log_in}
-                </Button>
-              </Form.Item>
-              <div className='form-group'>
-                <div className='link-action'>
-                  <p> {strings.no_account}</p>
-                  <Link to="/register">{strings.register_now}</Link>
-                </div>
-              </div>
-            </Form>
-          </div>
+                {strings.log_in}
+              </Button>
+            </Form.Item>
+            <div className='link-action'>
+              <p>Don't have an account?</p>
+              <Link to='/register'>Register now!</Link>
+            </div>
+          </Form>
         </div>
       </div>
+    </div>
     </div>
   )
 }
