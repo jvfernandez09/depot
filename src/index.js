f1a0ad0ae4cfba2c40cea880e2a6f90ef8c8057b
Jose Fernandez <jose@proudcloud.net>
2019-11-28 17:57:28 +0800

    Add apollo link retry on xem balance query and add xem api to blockchain

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { concat } from 'apollo-link'
 import { ApolloClient } from "apollo-client"
 import { ApolloProvider } from "react-apollo"
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { RetryLink } from 'apollo-link-retry'
 import { RestLink } from 'apollo-link-rest'
 import { setContext } from 'apollo-link-context'
 
@@ -21,6 +22,19 @@ const API = process.env.REACT_APP_ENV === 'staging' || process.env.REACT_APP_ENV
 const TOKEN_DEPOT_API = process.env.REACT_APP_ENV === 'staging' || process.env.REACT_APP_ENV === 'development' ? process.env.REACT_APP_GWX_STAGING_API_URL
 : process.env.REACT_APP_GWX_PROD_API_URL
 
+
+const retry = new RetryLink({
+  delay: {
+    initial: 300,
+    max: Infinity,
+    jitter: true
+  },
+  attempts: {
+    max: 5,
+    retryIf: (error, _operation) => !!error
+  }
+});
+
 const restLink = new RestLink({
   uri: API,
   endpoints: {
@@ -34,6 +48,8 @@ const restLink = new RestLink({
   fieldNameDenormalizer: key => humps.decamelize(key)
 })
 
+const link = concat(retry, restLink)
+
 const authLink = setContext((_, { headers }) => {
   const token = window.localStorage.getItem('AUTH_TOKEN')
   return {
@@ -45,7 +61,7 @@ const authLink = setContext((_, { headers }) => {
 })
 
 const client = new ApolloClient({
-  link: authLink.concat(restLink),
+  link: authLink.concat(link),
   cache: new InMemoryCache()
 })
 
